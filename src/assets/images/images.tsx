a536537c20c1357185b5f5d62a91e5a49122bf30
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-07-05 21:01:43 +0300

    feat: added images for about page

--- src/assets/images/images.tsx
+++ src/assets/images/images.tsx
@@ -5,4 +5,6 @@ import card1 from "./card1.png";
 import card2 from "./card2.png";
 import card3 from "./card3.png";
 import card4 from "./card4.png";
-export { hero3, heroImage, Heroimage2, card1, card2, card3, card4 };
+import handshake from "./handshake.png";
+import teamSuccess from "./team.png";
+export { hero3, heroImage, Heroimage2, card1, card2, card3, card4, handshake, teamSuccess };
