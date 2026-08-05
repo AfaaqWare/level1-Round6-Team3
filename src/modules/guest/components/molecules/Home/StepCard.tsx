c66b74eee60372fe0e484c4d3491f698a8441187
ZAHRAA26 <zahraa.elsayed26@yahoo.com>
2026-08-05 15:02:46 +0300

    feat(ui): replace title with description

--- src/modules/guest/components/molecules/Home/StepCard.tsx
+++ src/modules/guest/components/molecules/Home/StepCard.tsx
@@ -2,19 +2,19 @@ import Title from "@/shared/components/atoms/Title";
 import Text from "@/shared/components/atoms/Text";
 
 interface StepCardProps {
-  title?: string;
-  text?: string;
+  title: string;
+  description: string;
 }
 
-export default function StepCard({ title = "title", text = "text" }: StepCardProps) {
+export default function StepCard({ title, description }: StepCardProps) {
   return (
     <div className="ds-bg-card ds-border-card ds-rounded-md ds-shadow-card flex flex-col gap-3 py-9 text-center">
       <Title size="md" variant="alt" isCenter className="ds-font-heading mx-auto font-semibold">
         {title}
       </Title>
 
-      <Text size="base" variant="disabled" isCenter className="mx-auto px-8 font-sans font-medium">
-        {text}
+      <Text size="base" variant="disabled" isCenter className="mx-auto px-8 font-medium">
+        {description}
       </Text>
     </div>
   );
