ac744dfd797c4a869e062a24ad0d860270eb3c74
ZAHRAA26 <zahraa.elsayed26@yahoo.com>
2026-08-05 16:09:02 +0300

    feat(ui): rename textKey to descriptionKey

--- src/modules/guest/utils/data.tsx
+++ src/modules/guest/utils/data.tsx
@@ -23,16 +23,16 @@ export const steps = [
   {
     id: 1,
     titleKey: "StepCard1.title",
-    textKey: "StepCard1.description",
+    descriptionKey: "StepCard1.description",
   },
   {
     id: 2,
     titleKey: "StepCard2.title",
-    textKey: "StepCard2.description",
+    descriptionKey: "StepCard2.description",
   },
   {
     id: 3,
     titleKey: "StepCard3.title",
-    textKey: "StepCard3.description",
+    descriptionKey: "StepCard3.description",
   },
 ];
