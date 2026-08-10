c96d9233e080b474de87a86888832a4e7b90991b
SaharMahmoudDev <elzerossss@gmail.com>
2026-08-10 13:58:46 +0300

    feat(auth-signIn): improve sign-in error handling, replace Sonner, add translations

--- src/modules/auth/api/ApiSignIn.ts
+++ src/modules/auth/api/ApiSignIn.ts
@@ -1,4 +1,19 @@
-import apiClient from "@/services/ApiClient";
 import { SignInFormData } from "@/validator/authValidation";
-import { AuthResponse } from "@/modules/auth/types/auth";
-export const signIn = (data: SignInFormData) => apiClient.post<AuthResponse>("/auth/login", data);
+import axios from "axios";
+import { AuthResponse } from "../types/auth";
+import apiClient from "@/services/ApiClient";
+import { SignInError } from "@/core/errors/SignInError";
+
+export async function signIn(data: SignInFormData) {
+  try {
+    const response = await apiClient.post<AuthResponse>("/auth/login", data);
+
+    return response;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new SignInError("Login failed", error.response?.status, error.response?.data?.message);
+    }
+
+    throw error;
+  }
+}
--- src/shared/components/templates/AuthPanel.tsx
+++ src/shared/components/templates/AuthPanel.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function AuthPanel({ children }: Props) {
   return (
-    <section className="ds-bg flex w-full flex-2 flex-col justify-center px-3 py-6">
+    <section className="ds-bg flex w-full flex-2 flex-col justify-center px-3 py-3 md:py-6">
       <div className="mx-auto w-full max-w-md">{children}</div>
     </section>
   );
