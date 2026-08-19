6861204eb465ae4ffab3cf7b39de7bbf05bd44fa
ZAHRAA26 <zahraa.elsayed26@yahoo.com>
2026-08-19 22:19:01 +0300

    feat(ui): edit translations

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { buildSeo } from "@/core/seo/Seo";
 import ContainerDashboardPage from "@/modules/dashboard/components/templates/ContainerDashboardPage";
+import DashboardSidebar from "@/modules/dashboard/components/organisms/DashboardSidebar";
 export const metadata = buildSeo({
   title: "Dashboard - Afaaq Ware",
   description: "User dashboard to manage profile and activities",
 });
 
 export default function DashboardPage() {
-  return <ContainerDashboardPage />;
+  return (
+    <div className="flex">
+      <DashboardSidebar />
+      <ContainerDashboardPage />
+    </div>
+  );
 }
--- src/modules/dashboard/components/organisms/DashboardMenu.tsx
+++ src/modules/dashboard/components/organisms/DashboardMenu.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import SidebarSection from "../molecules/SidebarSection";
 import {
   Users,
@@ -16,86 +18,92 @@ import { useTranslations } from "next-intl";
 import Image from "next/image";
 
 export default function DashboardMenu() {
-  const t = useTranslations("dashboard.menu");
+  const t = useTranslations("dashboard.dashboard");
 
   return (
     <nav className="my-6 flex w-full flex-1 flex-col gap-6 text-left">
+      {/* Dashboard */}
       <div className="flex w-full items-center gap-3 rounded-xl bg-[var(--color-primary-200)] py-3 pl-6 text-[var(--color-primary)]">
         <Image src={group} alt="Dashboard" width={20} height={20} />
-        <h4 className="font-semibold">{t("dashboard")}</h4>
+
+        <h4 className="font-semibold">{t("menu.dashboard")}</h4>
       </div>
 
       <hr className="border-0 border-t border-[var(--border-color)]" />
 
+      {/* User Management */}
       <SidebarSection
-        title={t("userManagement.title")}
+        title={t("menu.userManagement.title")}
         items={[
           {
-            label: t("userManagement.allUsers"),
+            label: t("menu.userManagement.allUsers"),
             icon: <Users size={22} />,
             href: "/dashboard/users",
           },
           {
-            label: t("userManagement.rolesPermission"),
+            label: t("menu.userManagement.rolesPermission"),
             icon: <ShieldCheck size={22} />,
             href: "/dashboard/roles",
           },
         ]}
       />
 
+      {/* Survey Management */}
       <SidebarSection
-        title={t("surveyManagement.title")}
+        title={t("menu.surveyManagement.title")}
         items={[
           {
-            label: t("surveyManagement.allSurveys"),
+            label: t("menu.surveyManagement.allSurveys"),
             icon: <ClipboardList size={22} />,
             href: "/dashboard/surveys",
           },
           {
-            label: t("surveyManagement.draftSurveys"),
+            label: t("menu.surveyManagement.draftSurveys"),
             icon: <FilePenLine size={22} />,
             href: "/dashboard/surveys/drafts",
           },
           {
-            label: t("surveyManagement.publishedSurveys"),
+            label: t("menu.surveyManagement.publishedSurveys"),
             icon: <Send size={22} />,
             href: "/dashboard/surveys/published",
           },
           {
-            label: t("surveyManagement.closedSurveys"),
+            label: t("menu.surveyManagement.closedSurveys"),
             icon: <LockKeyhole size={22} />,
             href: "/dashboard/surveys/closed",
           },
         ]}
       />
 
+      {/* Response Management */}
       <SidebarSection
-        title={t("responseManagement.title")}
+        title={t("menu.responseManagement.title")}
         items={[
           {
-            label: t("responseManagement.responses"),
+            label: t("menu.responseManagement.responses"),
             icon: <PieChart size={22} />,
             href: "/dashboard/responses",
           },
           {
-            label: t("responseManagement.analytics"),
+            label: t("menu.responseManagement.analytics"),
             icon: <ChartNoAxesCombined size={22} />,
             href: "/dashboard/analytics",
           },
         ]}
       />
 
+      {/* Others */}
       <div className="border-t border-[var(--border-color)] pt-6">
         <SidebarSection
-          title={t("others.title")}
+          title={t("menu.others.title")}
           items={[
             {
-              label: t("others.profile"),
+              label: t("menu.others.profile"),
               icon: <User size={22} />,
               href: "/dashboard/profile",
             },
             {
-              label: t("others.logout"),
+              label: t("menu.others.logout"),
               icon: <LogOut size={22} />,
               href: "#",
               danger: true,
