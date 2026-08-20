f982ad87b9d0db4e69d600b108ebc617c48db6c3
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-08-20 12:52:51 +0300

    feat(dashbaord): add sidebar for navigation

--- src/shared/components/templates/DashboardLayout.tsx
+++ src/shared/components/templates/DashboardLayout.tsx
@@ -10,6 +10,7 @@ import { Bell, ListMinus } from "@/assets/icons/icons";
 import NavIconButton from "../atoms/navbar/NavIconButton";
 import MobileNavHeader from "../atoms/navbar/MobileNavHeader";
 import { cn } from "@/lib/cn";
+import DashboardSidebar from "@/modules/dashboard/components/organisms/DashboardSidebar";
 
 interface Props {
   children: React.ReactNode;
@@ -78,9 +79,14 @@ export default function DashboardLayout({ children }: Props) {
         </div>
       </div>
 
-      <main className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        {children}
-      </main>
+      <div className="ds-container mx-auto flex gap-6 px-4 sm:px-6 lg:px-8">
+        {/* Desktop Sidebar */}
+        <aside className="hidden h-[calc(100vh-4rem)] w-1/4 overflow-y-auto border-r border-[var(--border-color)] py-6 md:block">
+          <DashboardSidebar />
+        </aside>
+
+        <main className="flex-1 py-6">{children}</main>
+      </div>
     </div>
   );
 }
