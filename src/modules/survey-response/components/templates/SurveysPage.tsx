3436e201a2a05c5081b06e22aa83dd054d23e1e7
ZAHRAA26 <zahraa.elsayed26@yahoo.com>
2026-09-14 17:02:43 +0300

    feat(ui): add all

--- src/modules/survey-response/components/templates/SurveysPage.tsx
+++ src/modules/survey-response/components/templates/SurveysPage.tsx
@@ -5,21 +5,16 @@ import { useState } from "react";
 import SurveysHeader from "../orgamisms/SurveysHeader";
 import SurveysToolbar from "../orgamisms/SurveysToolbar";
 import SurveysGrid from "../orgamisms/SurveysGrid";
+import NoSurvey from "../orgamisms/NoSurvey";
 
 import type { SurveyStatusFilter } from "../molecules/SurveyStatusTabs";
 import type { SurveysSortOrder } from "../molecules/SurveySortSelect";
 
-
-import NoSurvey from "../orgamisms/NoSurvey";
 import useGetAllSurveys from "../../hooks/useGetAllSurveys";
-
-export default function SurveysPage() {
-  const [status, setStatus] = useState<SurveyStatusFilter>("all");
-
 
 interface SurveysPageProps {
   initialStatus?: SurveyStatusFilter;
-
+}
 
 export default function SurveysPage({ initialStatus = "all" }: SurveysPageProps) {
   const [status, setStatus] = useState<SurveyStatusFilter>(initialStatus);
@@ -56,9 +51,7 @@ export default function SurveysPage({ initialStatus = "all" }: SurveysPageProps)
           <SurveysGrid status={status} search={search} sort={sort} />
         </>
       ) : (
-        <>
-          <NoSurvey />
-        </>
+        <NoSurvey />
       )}
     </div>
   );
