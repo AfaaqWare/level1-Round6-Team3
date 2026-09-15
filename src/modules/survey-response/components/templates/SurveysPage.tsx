c44e01fa3c23de203ac4f266b9ff7ddb6010c339
Abdallah Aziz <abdullah.229op@gmail.com>
2026-09-15 16:41:14 +0300

    Refactor SurveysPage to remove initialStatus prop

--- src/modules/survey-response/components/templates/SurveysPage.tsx
+++ src/modules/survey-response/components/templates/SurveysPage.tsx
@@ -5,25 +5,24 @@ import { useState } from "react";
 import SurveysHeader from "../orgamisms/SurveysHeader";
 import SurveysToolbar from "../orgamisms/SurveysToolbar";
 import SurveysGrid from "../orgamisms/SurveysGrid";
-import NoSurvey from "../orgamisms/NoSurvey";
 
 import type { SurveyStatusFilter } from "../molecules/SurveyStatusTabs";
 import type { SurveysSortOrder } from "../molecules/SurveySortSelect";
 
+import NoSurvey from "../orgamisms/NoSurvey";
 import useGetAllSurveys from "../../hooks/useGetAllSurveys";
 
-interface SurveysPageProps {
-  initialStatus?: SurveyStatusFilter;
-}
+export default function SurveysPage() {
+  const [status, setStatus] = useState<SurveyStatusFilter>("all");
 
-export default function SurveysPage({ initialStatus = "all" }: SurveysPageProps) {
-  const [status, setStatus] = useState<SurveyStatusFilter>(initialStatus);
   const [search, setSearch] = useState("");
+
   const [sort, setSort] = useState<SurveysSortOrder>("newest");
 
   const { data, isLoading } = useGetAllSurveys();
 
   const surveys = data?.data ?? [];
+
   const hasSurveys = surveys.length > 0;
 
   return (
