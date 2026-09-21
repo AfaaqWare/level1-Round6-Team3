edbf4f74f49c6c9636ba15834b9647f8758ae11a
Doniamagdy <donallino@gmail.com>
2026-09-21 10:21:43 +0300

    refactor: replace xlsx with exceljs

--- src/modules/responses/components/organismas/ResponseHeader.tsx
+++ src/modules/responses/components/organismas/ResponseHeader.tsx
@@ -5,7 +5,8 @@ import Text from "@/shared/components/atoms/Text";
 import Title from "@/shared/components/atoms/Title";
 import React from "react";
 import useGetAllResponses from "@/modules/responses/hooks/useGetAllResponses";
-import * as XLSX from "xlsx";
+// import * as XLSX from "xlsx";
+import ExcelJS from "exceljs";
 import { useTranslations } from "next-intl";
 
 function ResponseHeader() {
@@ -13,24 +14,62 @@ function ResponseHeader() {
 
   const { data } = useGetAllResponses();
 
-  const handleExport = () => {
-    const exportData = (data?.data ?? []).map((response, index) => ({
-      ID: index + 1,
-      Respondent: response.respondentName,
-      Email: response.respondentEmail,
-      "Survey ID": response.surveyId,
-      Answers: Object.keys(response.answers).length,
-      "Submitted At": response.submittedAt,
-    }));
+  // const handleExport = () => {
+  //   const exportData = (data?.data ?? []).map((response, index) => ({
+  //     ID: index + 1,
+  //     Respondent: response.respondentName,
+  //     Email: response.respondentEmail,
+  //     "Survey ID": response.surveyId,
+  //     Answers: Object.keys(response.answers).length,
+  //     "Submitted At": response.submittedAt,
+  //   }));
 
-    const worksheet = XLSX.utils.json_to_sheet(exportData);
+  //   const worksheet = XLSX.utils.json_to_sheet(exportData);
 
-    const workbook = XLSX.utils.book_new();
+  //   const workbook = XLSX.utils.book_new();
 
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Responses");
+  //   XLSX.utils.book_append_sheet(workbook, worksheet, "Responses");
 
-    XLSX.writeFile(workbook, "responses.xlsx");
-  };
+  //   XLSX.writeFile(workbook, "responses.xlsx");
+  // };
+
+  const handleExport = async () => {
+  const exportData = (data?.data ?? []).map((response, index) => ({
+    ID: index + 1,
+    Respondent: response.respondentName,
+    Email: response.respondentEmail,
+    "Survey ID": response.surveyId,
+    Answers: Object.keys(response.answers).length,
+    "Submitted At": response.submittedAt,
+  }));
+
+  if (exportData.length === 0) return;
+
+  const workbook = new ExcelJS.Workbook();
+  const worksheet = workbook.addWorksheet("Responses");
+
+  worksheet.addRow(Object.keys(exportData[0]));
+
+  exportData.forEach((row) => {
+    worksheet.addRow(Object.values(row));
+  });
+
+  const buffer = await workbook.xlsx.writeBuffer();
+
+  const blob = new Blob([buffer], {
+    type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+  });
+
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement("a");
+
+  link.href = url;
+  link.download = "responses.xlsx";
+  link.click();
+
+  URL.revokeObjectURL(url);
+};
+
   return (
     <main className="flex justify-between">
       <section>
