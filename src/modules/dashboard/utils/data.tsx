ebffc30a9c7b7576615ed12344693d6327c58d47
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-09-25 00:11:46 +0300

    feat(dashboard): show manage-users quick action to admins only

--- src/modules/dashboard/utils/data.tsx
+++ src/modules/dashboard/utils/data.tsx
@@ -37,6 +37,7 @@ export const quickActions = [
     href: "/surveys/create-survey",
     variant: "teal" as const,
     icon: "plus" as const,
+    adminOnly: false,
   },
   {
     id: "manage-users",
@@ -44,6 +45,7 @@ export const quickActions = [
     href: "dashboard/users",
     variant: "green" as const,
     icon: "users" as const,
+    adminOnly: true,
   },
   {
     id: "view-responses",
@@ -51,6 +53,7 @@ export const quickActions = [
     href: "/dashboard/all-responses",
     variant: "purple" as const,
     icon: "pieChart" as const,
+    adminOnly: false,
   },
   {
     id: "export-reports",
@@ -58,6 +61,7 @@ export const quickActions = [
     href: "/reports",
     variant: "blue" as const,
     icon: "download" as const,
+    adminOnly: false,
   },
 ];
 export const theme = {
