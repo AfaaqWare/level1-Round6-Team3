c118d40aec5201af99371076cc76c930c2ae625b
abdullahMohamed13 <abdullah.229op@gmail.com>
2026-09-25 00:11:34 +0300

    chore(config): remove duplicate hardcoded SITE_URL

--- src/config/env.ts
+++ src/config/env.ts
@@ -18,6 +18,4 @@ export const ENV = {
   REFRESH_TOKEN_KEY: process.env.NEXT_PUBLIC_REFRESH_TOKEN_KEY || "refreshToken",
 
   DISABLE_DASHBOARD_PROTECTION: process.env.NEXT_PUBLIC_DISABLE_DASHBOARD_PROTECTION === "true",
-
-  SITE_URL:"localhost:3000"
 };
